941021849ca0acb37449c868cbee2b8576cebe4e
Jake Fitzgerald <jakehenryfitz@Jakes-MacBook-Pro-2.local>
2017-04-03 17:13:54 +1000

    fix

--- app.py
+++ app.py
@@ -1,32 +1,59 @@
 from flask import Flask, request
+import json
 import requests
- 
+
 app = Flask(__name__)
- 
-ACCESS_TOKEN = "EAAF4fQe12XQBAOfF2ZBVTBy9TfrBJaKSl40H1Q0fYJfUZABOXiCYw2hmwEVMjGUXGSqzBQEOezNIe6t3GEv19izkD08dlgPIYcnavkIeZBFDfc15aShAMnCLBDgIpYC3e9ViYqLZAWexLaDqA9WPJxsSLXZBvFUwdplsUtlamoQZDZD"
- 
+
+# This needs to be filled with the Page Access Token that will be provided
+# by the Facebook App that will be created.
+PAT = 'EAAF4fQe12XQBAOfF2ZBVTBy9TfrBJaKSl40H1Q0fYJfUZABOXiCYw2hmwEVMjGUXGSqzBQEOezNIe6t3GEv19izkD08dlgPIYcnavkIeZBFDfc15aShAMnCLBDgIpYC3e9ViYqLZAWexLaDqA9WPJxsSLXZBvFUwdplsUtlamoQZDZD'
+
 @app.route('/', methods=['GET'])
 def handle_verification():
-    return request.args['hub.challenge']
- 
-def reply(user_id, msg):
-    data = {
-        "recipient": {"id": user_id},
-        "message": {"text": msg}
-    }
-    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
-    print(resp.content)
- 
- 
+  print "Handling Verification."
+  if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
+    print "Verification successful!"
+    return request.args.get('hub.challenge', '')
+  else:
+    print "Verification failed!"
+    return 'Error, wrong validation token'
+
 @app.route('/', methods=['POST'])
-def handle_incoming_messages():
-    data = request.json
-    sender = data['entry'][0]['messaging'][0]['sender']['id']
-    message = data['entry'][0]['messaging'][0]['message']['text']
-    reply(sender, message[::-1])
- 
-    return "ok"
- 
- 
+def handle_messages():
+  print "Handling Messages"
+  payload = request.get_data()
+  print payload
+  for sender, message in messaging_events(payload):
+    print "Incoming from %s: %s" % (sender, message)
+    send_message(PAT, sender, message)
+  return "ok"
+
+def messaging_events(payload):
+  """Generate tuples of (sender_id, message_text) from the
+  provided payload.
+  """
+  data = json.loads(payload)
+  messaging_events = data["entry"][0]["messaging"]
+  for event in messaging_events:
+    if "message" in event and "text" in event["message"]:
+      yield event["sender"]["id"], event["message"]["text"].encode('unicode_escape')
+    else:
+      yield event["sender"]["id"], "I can't echo this"
+
+
+def send_message(token, recipient, text):
+  """Send the message text to recipient with id recipient.
+  """
+
+  r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+    params={"access_token": token},
+    data=json.dumps({
+      "recipient": {"id": recipient},
+      "message": {"text": text.decode('unicode_escape')}
+    }),
+    headers={'Content-type': 'application/json'})
+  if r.status_code != requests.codes.ok:
+    print r.text
+
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run()
